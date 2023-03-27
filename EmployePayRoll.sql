837777ae98ec04aa4509cfb98d305b1cb7bd84a0
koshtirushikesh <117973034+koshtirushikesh@users.noreply.github.com>
2023-03-27 18:06:53 +0530

    [Rushi]: E R Diagram

--- EmployePayRoll.sql
+++ EmployePayRoll.sql
@@ -34,11 +34,68 @@ ALTER TABLE EmployeePayRoll_Table ADD Gender CHAR(1)
 UPDATE EmployeePayRoll_Table SET Gender = 'M' WHERE EmployeeName = 'Rushi'
 UPDATE EmployeePayRoll_Table SET Gender = 'M' WHERE EmployeeName = 'Shubham'
 UPDATE EmployeePayRoll_Table SET Gender = 'F' WHERE EmployeeName = 'Cristen'
-
---UC7 Sum,Average,Min,Max Count
+   
+-- UC7 Sum,Average,Min,Max Count
 SELECT Gender , SUM(EmployeeSalary) AS SumOfSalary FROM EmployeePayRoll_Table GROUP BY Gender 
 SELECT Gender , AVG(EmployeeSalary) AS AvgOfSalary FROM EmployeePayRoll_Table GROUP BY Gender 
 SELECT Gender , MIN(EmployeeSalary) AS MinOfSalary FROM EmployeePayRoll_Table GROUP BY Gender 
 SELECT Gender , MAX(EmployeeSalary) AS MaxOfSalary FROM EmployeePayRoll_Table GROUP BY Gender 
 
--- UC8  
+-- UC8 
+-------------------------------------
+ALTER TABLE EmployeePayRoll_Table  ADD PhoneNumber BIGINT
+ALTER TABLE EmployeePayRoll_Table  ADD Address VARCHAR(50)
+ALTER TABLE EmployeePayRoll_Table  ADD Department VARCHAR(50) NOT NULL
+
+-- UC9
+ALTER TABLE EmployeePayRoll_Table  ADD BasicPay INT
+ALTER TABLE EmployeePayRoll_Table  ADD Deductions INT
+ALTER TABLE EmployeePayRoll_Table  ADD TaxablePay INT
+ALTER TABLE EmployeePayRoll_Table  ADD IncomeTax INT
+ALTER TABLE EmployeePayRoll_Table  ADD NetPay INT
+
+-- UC10
+INSERT INTO EmployeePayRoll_Table (EmployeeName,EmployeeSalary,Gender,PhoneNumber,Department)
+VALUES ('Terissa',200000,'F',987654321,'HR'),
+('Terissa',200000,'F',987654321,'Sales')
+
+--
+
+CREATE TABLE Employee_Table
+(
+EmpId INT IDENTITY(1,1) PRIMARY KEY,
+EmpName VARCHAR(30),
+PhoneNumber BIGINT,
+Address VARCHAR(100),
+City VARCHAR(20),
+State VARCHAR(20),
+Gender CHAR(1),
+)
+
+SELECT * FROM Employee_Table
+INSERT INTO Employee_Table (EmpId) VALUES ()
+
+CREATE TABLE Department_Table
+(
+DepartmentId INT IDENTITY(1,1) PRIMARY KEY,
+DepartmentName VARCHAR(30),
+FOREIGN KEY (EmpId) REFERENCES Employee_Table(EmpId)
+)
+
+CREATE TABLE PayRoll_Table
+(
+FOREIGN KEY (EmpId) REFERENCES Employee_Table(EmpId),
+BasicPay INT,
+Deduction INT,
+TaxablePay INT,
+Tax INT,
+NetPay INT
+)
+
+CREATE TABLE Company_Table
+(
+CompanyId INT IDENTITY(1,1) PRIMARY KEY,
+CompanyName VARCHAR(30),
+FOREIGN KEY (EmpId) REFERENCES Employee_Table(EmpId),
+)
+
